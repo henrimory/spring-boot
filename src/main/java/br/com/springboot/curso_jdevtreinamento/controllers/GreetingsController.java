ae164266c31ff1a1ea25ccfa317abbb9e2363baf
Henrique Oliveira <henrique.mory@hotmail.com>
2022-03-29 23:54:05 -0300

    Aprendendo Spring boot API listatodos!

--- src/main/java/br/com/springboot/curso_jdevtreinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdevtreinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_jdevtreinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +49,13 @@ public class GreetingsController {
     	
     	return "Olá mundo " + nome;    	
     	
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();//executa a consulta no banco de dados
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);//retorna a lista em json
     }
 }
